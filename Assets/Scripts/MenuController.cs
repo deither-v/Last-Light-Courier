9e6e9b40b46247362d293efba1fcef4540be1345
TristhanOrbito <tristhan110904@gmail.com>
2026-03-28 19:42:42 +0800

    update

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; // This line is required!
+using UnityEngine.SceneManagement; 
 
 public class MenuController : MonoBehaviour
 {
@@ -7,5 +7,28 @@ public class MenuController : MonoBehaviour
     {
         // Replace "Level 1" with the exact name of your scene file
         SceneManager.LoadScene("Level 1");
+    }
+
+    // NEW FUNCTION
+    public void QuitGame()
+    {
+        Debug.Log("Game is exiting..."); // This shows in the Console to prove it works
+        Application.Quit(); // This closes the actual app after it is built
+    }
+    public GameObject mainMenuArea; // Drag your Main Menu buttons group here
+    public GameObject optionsPanel; // Drag your Options Panel here
+
+    // This opens Options and hides the Main Buttons
+    public void OpenOptions()
+    {
+        mainMenuArea.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    // This goes back to the Main Buttons
+    public void CloseOptions()
+    {
+        mainMenuArea.SetActive(true);
+        optionsPanel.SetActive(false);
     }
 }
